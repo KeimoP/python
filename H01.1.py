a31c7d3cdfe33418a04bfa39b57fce69c244b079
KeimoP <keimo@hkhk.edu.ee>
2024-10-07 15:10:58 +0300

    H01.1

---
+++ H01.1.py
@@ -0,0 +1,49 @@
+#Keimo Koolist
+#Harjutus 1,1
+
+import turtle
+
+#positsioon
+turtle.penup()
+turtle.goto(-400,200)
+turtle.pendown()
+ 
+# Kolmnurk
+turtle.fd(200)
+turtle.left(120)
+turtle.fd(200)
+turtle.left(120)
+turtle.fd(200)
+
+#K Pos
+turtle.penup()
+turtle.goto(-100,200)
+turtle.pendown()
+
+#K-täht
+turtle.left(210)
+turtle.fd(180)
+turtle.right(90)
+turtle.fd(30)
+turtle.right(90)
+turtle.fd(90)
+turtle.left(135)
+turtle.fd(125)
+turtle.right(90)
+turtle.fd(30)
+turtle.right(90)
+turtle.fd(125)
+turtle.left(90)
+turtle.fd(75)
+turtle.right(90)
+turtle.fd(30)
+turtle.right(90)
+turtle.fd(75)
+turtle.left(135)
+turtle.fd(50)
+turtle.right(90)
+turtle.fd(30)
+
+
+#Turtle lõpp
+turtle.done()
