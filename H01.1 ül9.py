c227fe96a3f49cc5020fc196a125631976f6cf4d
KeimoP <keimo@hkhk.edu.ee>
2024-10-07 15:31:35 +0300

    H01.1 9 Pilt

---
+++ H01.1 ül9.py
@@ -0,0 +1,28 @@
+#Keimo Koolist
+#ül 1.1, 9 joonis
+
+import turtle
+
+#Ristmaja
+turtle.fd(100)
+turtle.left(90)
+turtle.fd(100)
+turtle.left(45)
+turtle.fd(70)
+turtle.left(90)
+turtle.fd(70)
+turtle.left(135)
+turtle.fd(100)
+turtle.fd(-100)
+turtle.right(90)
+turtle.fd(100)
+turtle.left(135)
+turtle.fd(140)
+turtle.right(135)
+turtle.fd(100)
+turtle.right(135)
+turtle.fd(140)
+
+#Turtle lõpp
+turtle.done()
+
